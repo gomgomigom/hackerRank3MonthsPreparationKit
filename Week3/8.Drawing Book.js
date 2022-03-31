a381e40b1af8fd0d661f881148003fe3f68d107e
gomgomigom <nodam89@naver.com>
2022-03-31 21:22:58 +0900

    Week3 8.Drawing Book.js Update

--- Week3/8.Drawing Book.js
+++ Week3/8.Drawing Book.js
@@ -1,13 +1,20 @@
 'use strict';
 
+// function pageCount(n, p) {
+//   const page = Math.floor(1 + n / 2);
+//   const wantFindPage = 1 + Math.floor(p / 2);
+//   const deltaPage = page - wantFindPage;
+//   console.log(
+//     `page: ${page}, wantFindPage: ${wantFindPage}, deltaPage: ${deltaPage}`
+//   );
+//   const result = wantFindPage > deltaPage ? deltaPage : wantFindPage - 1;
+//   return result;
+// }
+
 function pageCount(n, p) {
-  const page = Math.floor(1 + n / 2);
-  const wantFindPage = 1 + Math.floor(p / 2);
-  const deltaPage = page - wantFindPage;
-  console.log(
-    `page: ${page}, wantFindPage: ${wantFindPage}, deltaPage: ${deltaPage}`
-  );
-  const result = wantFindPage > deltaPage ? deltaPage : wantFindPage - 1;
+  const pageMaxIndex = Math.floor(n / 2);
+  const targetIndex = Math.floor(p / 2);
+  const result = Math.min(targetIndex, pageMaxIndex - targetIndex);
   return result;
 }
 
