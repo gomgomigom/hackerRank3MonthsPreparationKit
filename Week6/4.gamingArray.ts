75279a338be8c6df60fea5f12eb44d8c0f3d7932
gomgomigom <nodam89@naver.com>
2022-05-08 09:07:06 +0900

    Week6 4.gamingArray.ts: Time limit exceeded

---
+++ Week6/4.gamingArray.ts
@@ -0,0 +1,16 @@
+function gamingArray(arr: number[]): string | undefined {
+  let i: number = 0;
+  while (arr.length > 0) {
+    const maxIndex: number = arr.indexOf(Math.max(...arr));
+    i++;
+    arr.splice(maxIndex);
+  }
+  return i % 2 === 1 ? 'BOB' : 'ANDY';
+}
+
+console.log(gamingArray([5, 2, 6, 3, 4]));
+console.log(gamingArray([3, 1]));
+
+let itt = [1, 2, 3];
+itt.splice(0);
+console.log(itt);
